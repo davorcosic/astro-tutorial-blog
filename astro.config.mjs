42cc39850752400c827e4e48a1cd73cbe2c11488
Davor Ćosić <davor.cosic@bulbtech.com>
2024-10-04 22:20:25 +0200

    Additional Astro API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Add the site property to the Astro config with your site’s own unique Netlify URL.
+    site: "https://example.com"
+});
